5ea15a2b2a8574857379669d4c7ebbe2bc820c64
Nat Chen <chen_natalie@outlook.com>
2025-01-31 10:34:48 +0800

    chore: add necessary comment

--- client/src/pages/Chat/index.tsx
+++ client/src/pages/Chat/index.tsx
@@ -1,275 +1,342 @@
 import { Button, Input, message, Popover } from "antd";
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
-import './index.scss';
+import "./index.scss";
 import { chatHistoryList, chatroomList, favoriteAdd } from "../../interfaces";
 import { UserInfo } from "../UpdateInfo";
 import TextArea from "antd/es/input/TextArea";
 import { useLocation } from "react-router-dom";
 import EmojiPicker from "@emoji-mart/react";
-import data from '@emoji-mart/data'
+import data from "@emoji-mart/data";
 import { UploadModal } from "./UploadModal";
 
 interface JoinRoomPayload {
-    chatroomId: number
-    userId: number
+  chatroomId: number;
+  userId: number;
 }
 
 interface SendMessagePayload {
-    sendUserId: number;
-    chatroomId: number;
-    message: Message
+  sendUserId: number;
+  chatroomId: number;
+  message: Message;
 }
 
-type MessageType = 'image' | 'text' | 'file';
+type MessageType = "image" | "text" | "file";
 
 interface Message {
-    type: MessageType
-    content: string
+  type: MessageType;
+  content: string;
 }
 
-type Reply  = {
-    type: 'sendMessage'
-    userId: number
-    message: ChatHistory
-} | {
-    type: 'joinRoom'
-    userId: number
-}
-
+type Reply =
+  | {
+      type: "sendMessage";
+      userId: number;
+      message: ChatHistory;
+    }
+  | {
+      type: "joinRoom";
+      userId: number;
+    };
 
 interface Chatroom {
-    id: number;
-    name: string;
-    createTime: Date;
+  id: number;
+  name: string;
+  createTime: Date;
 }
 
 interface ChatHistory {
-    id: number
-    content: string
-    type: number
-    chatroomId: number
-    senderId: number
-    createTime: Date,
-    sender: UserInfo
+  id: number;
+  content: string;
+  type: number;
+  chatroomId: number;
+  senderId: number;
+  createTime: Date;
+  sender: UserInfo;
 }
 
 interface User {
-    id: number;
-    email: string;
-    headPic: string;
-    nickName: string;
-    username: string;
-    createTime: Date;
+  id: number;
+  email: string;
+  headPic: string;
+  nickName: string;
+  username: string;
+  createTime: Date;
 }
 
 export function getUserInfo(): User {
-    return JSON.parse(localStorage.getItem('userInfo')!);
+  return JSON.parse(localStorage.getItem("userInfo")!);
 }
 
 export function Chat() {
-    const socketRef = useRef<Socket>();
-    const [roomId, setChatroomId] = useState<number>();
-    const userInfo = getUserInfo();
-    const [isUploadModalOpen, setUploadModalOpen] = useState(false);
+  const socketRef = useRef<Socket>();
+  const [roomId, setChatroomId] = useState<number>();
+  const userInfo = getUserInfo();
+  const [isUploadModalOpen, setUploadModalOpen] = useState(false);
 
-    useEffect(() => {
-        if(!roomId) {
-            return;
-        }
-        const socket = socketRef.current = io('http://localhost:3005');
-        socket.on('connect', function() {
-    
-            const payload: JoinRoomPayload = {
-                chatroomId: roomId,
-                userId: userInfo.id
-            }
-    
-            socket.emit('joinRoom', payload);
-    
-            socket.on('message', (reply: Reply) => {
-                if(reply.type === 'sendMessage') {
-                    setChatHistory((chatHistory) => {
-                        return chatHistory ? [...chatHistory, reply.message] : [reply.message]
-                    });   
-                    setTimeout(() => {
-                        document.getElementById('bottom-bar')?.scrollIntoView({block: 'end'});
-                    }, 300);
-                }
-            });
-        });
-        return () => {
-            socket.disconnect();
-        }
-    }, [roomId]);
+  useEffect(() => {
+    if (!roomId) {
+      return;
+    }
+    const socket = (socketRef.current = io("http://localhost:3005"));
+    // 建立与服务器的连接，两种消息类型：1. 加入房间 joinRoom 2. 发送消息 sendMessage
+    socket.on("connect", function () {
+      const payload: JoinRoomPayload = {
+        chatroomId: roomId,
+        userId: userInfo.id,
+      };
 
-    function sendMessage(value: string, type: MessageType = 'text') {
-        if(!value) {
-            return;
-        }
-        if(!roomId) {
-            return;
-        }
+      socket.emit("joinRoom", payload); // 发送加入房间的消息
 
-        const payload: SendMessagePayload = {
-            sendUserId: userInfo.id,
-            chatroomId: roomId,
-            message: {
-                type,
-                content: value
-            }
+      // 监听来自服务端发送的消息
+      socket.on("message", (reply: Reply) => {
+        console.log("here", reply);
+        if (reply.type === "sendMessage") {
+          setChatHistory((chatHistory) => {
+            return chatHistory
+              ? [...chatHistory, reply.message] // 历史消息 + 新消息
+              : [reply.message];
+          });
+          // 收到消息后滚动到底部
+          setTimeout(() => {
+            document
+              .getElementById("bottom-bar")
+              ?.scrollIntoView({ block: "end" });
+          }, 300);
         }
+      });
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, [roomId]);
 
-        socketRef.current?.emit('sendMessage', payload);
+  // 消息可能为文本、图片、文件，但最终都是字符串（图片与文件为 url 地址）
+  function sendMessage(value: string, type: MessageType = "text") {
+    if (!value) {
+      return;
+    }
+    if (!roomId) {
+      return;
     }
 
-    const [roomList, setRoomList] = useState<Array<Chatroom>>();
+    const payload: SendMessagePayload = {
+      sendUserId: userInfo.id,
+      chatroomId: roomId,
+      message: {
+        type,
+        content: value,
+      },
+    };
 
-    async function queryChatroomList() {
-        try{
-            const res = await chatroomList();
+    socketRef.current?.emit("sendMessage", payload); // 发送消息后，on("message") 监听到服务端发送过来的消息
+  }
 
-            if(res.status === 201 || res.status === 200) {
-                setRoomList(res.data.map((item: Chatroom) => {
-                    return {
-                        ...item,
-                        key: item.id
-                    }
-                }));
-            }
-        } catch(e: any){
-            console.log(e);
-            message.error(e.response?.data?.message || '系统繁忙，请稍后再试');
-        }
+  const [roomList, setRoomList] = useState<Array<Chatroom>>();
+
+  async function queryChatroomList() {
+    try {
+      const res = await chatroomList();
+
+      if (res.status === 201 || res.status === 200) {
+        setRoomList(
+          res.data.map((item: Chatroom) => {
+            return {
+              ...item,
+              key: item.id,
+            };
+          })
+        );
+      }
+    } catch (e: any) {
+      console.log(e);
+      message.error(e.response?.data?.message || "系统繁忙，请稍后再试");
     }
+  }
 
-    useEffect(() => {
-        queryChatroomList();
-    }, []);
+  useEffect(() => {
+    queryChatroomList();
+  }, []);
 
-    useEffect(() => {
-        setTimeout(() => {
-            document.getElementById('bottom-bar')?.scrollIntoView({block: 'end'});
-        }, 300);
-    }, [roomId])
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementById("bottom-bar")?.scrollIntoView({ block: "end" });
+    }, 300);
+  }, [roomId]);
 
-    const [chatHistory, setChatHistory] = useState<Array<ChatHistory>>();
+  const [chatHistory, setChatHistory] = useState<Array<ChatHistory>>();
 
-    async function queryChatHistoryList(chatroomId: number) {
-        try{
-            const res = await chatHistoryList(chatroomId);
+  async function queryChatHistoryList(chatroomId: number) {
+    try {
+      const res = await chatHistoryList(chatroomId);
 
-            if(res.status === 201 || res.status === 200) {
-                setChatHistory(res.data.map((item: Chatroom) => {
-                    return {
-                        ...item,
-                        key: item.id
-                    }
-                }));
-            }
-        } catch(e: any){
-            message.error(e.response?.data?.message || '系统繁忙，请稍后再试');
-        }
+      if (res.status === 201 || res.status === 200) {
+        setChatHistory(
+          res.data.map((item: Chatroom) => {
+            return {
+              ...item,
+              key: item.id,
+            };
+          })
+        );
+      }
+    } catch (e: any) {
+      message.error(e.response?.data?.message || "系统繁忙，请稍后再试");
     }
-    const [inputText, setInputText] = useState('');
+  }
+  const [inputText, setInputText] = useState("");
 
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        if(location.state?.chatroomId) {
-            setChatroomId(location.state?.chatroomId);
+  // 第二个 useEffect
+  useEffect(() => {
+    if (location.state?.chatroomId) {
+      setChatroomId(location.state?.chatroomId);
 
-            queryChatHistoryList(location.state?.chatroomId);
-        }
-    }, [location.state?.chatroomId]);
-
-    async function addToFavorite(chatHistoryId: number) {
-        try{
-            const res = await favoriteAdd(chatHistoryId);
-
-            if(res.status === 201 || res.status === 200) {
-                message.success('收藏成功')
-            }
-        } catch(e: any){
-            message.error(e.response?.data?.message || '系统繁忙，请稍后再试');
-        }
+      queryChatHistoryList(location.state?.chatroomId);
     }
+  }, [location.state?.chatroomId]);
 
-    const [uploadType, setUploadType] = useState<'image' | 'file'>('image'); 
+  async function addToFavorite(chatHistoryId: number) {
+    try {
+      const res = await favoriteAdd(chatHistoryId);
 
-    return <div id="chat-container">
-        <div className="chat-room-list">
-            {
-                roomList?.map(item => {
-                    return <div className={`chat-room-item ${item.id === roomId ? 'selected' : ''}`} key={item.id} data-id={item.id} onClick={() => {
-                        queryChatHistoryList(item.id);
-                        setChatroomId(item.id);
-                    }}
-                    >{item.name}</div>
-                })
-            }
-        </div>
-        <div className="message-list">
-            {chatHistory?.map(item => {
-                return <div 
-                    className={`message-item ${item.senderId === userInfo.id ? 'from-me' : ''}`} 
-                    key={item.id} 
-                    data-id={item.id}
-                    onDoubleClick={() => {
-                        addToFavorite(item.id)
-                    }}
-                    >
-                    <div className="message-sender">
-                        <img src={item.sender.headPic} />
-                        <span className="sender-nickname">{item.sender.nickName}</span>
-                    </div>
-                    <div className="message-content">
-                        {
-                            item.type === 0 
-                                ? item.content 
-                                : item.type === 1
-                                    ? <img src={item.content} style={{maxWidth: 200}}/>
-                                    : <div><a download href={item.content}>{item.content}</a></div>
-                        }
-                    </div>
-                </div>
-            })}
-            <div id="bottom-bar" key='bottom-bar'></div>
-        </div>
-        <div className="message-input">
-            <div className="message-type">
-                <div className="message-type-item" key={1}>
-                    <Popover content={<EmojiPicker data={data} onEmojiSelect={(emoji: any) => {
-                        setInputText((inputText) => inputText + emoji.native)
-                    }} />} title="Title" trigger="click">
-                        表情
-                    </Popover>
-                </div>
-                <div className="message-type-item" key={2} onClick={() => {
-                    setUploadType('image');
-                    setUploadModalOpen(true);
-                }}>图片</div>
-                <div className="message-type-item" key={3}onClick={() => {
-                    setUploadType('file');
-                    setUploadModalOpen(true);
-                }}>文件</div>
+      if (res.status === 201 || res.status === 200) {
+        message.success("收藏成功");
+      }
+    } catch (e: any) {
+      message.error(e.response?.data?.message || "系统繁忙，请稍后再试");
+    }
+  }
+
+  const [uploadType, setUploadType] = useState<"image" | "file">("image");
+
+  return (
+    <div id="chat-container">
+      <div className="chat-room-list">
+        {roomList?.map((item) => {
+          return (
+            <div
+              className={`chat-room-item ${
+                item.id === roomId ? "selected" : ""
+              }`}
+              key={item.id}
+              data-id={item.id}
+              onClick={() => {
+                queryChatHistoryList(item.id);
+                setChatroomId(item.id);
+              }}
+            >
+              {item.name}
             </div>
-            <div className="message-input-area">
-                <TextArea className="message-input-box" value={inputText} onChange={(e) => {
-                    setInputText(e.target.value)
-                }}/>
-                <Button className="message-send-btn" type="primary" onClick={() => {
-                    sendMessage(inputText)
-                    setInputText('');
-                }}>发送</Button>
+          );
+        })}
+      </div>
+      <div className="message-list">
+        {chatHistory?.map((item) => {
+          return (
+            <div
+              className={`message-item ${
+                item.senderId === userInfo.id ? "from-me" : ""
+              }`}
+              key={item.id}
+              data-id={item.id}
+              onDoubleClick={() => {
+                addToFavorite(item.id);
+              }}
+            >
+              <div className="message-sender">
+                <img src={item.sender.headPic} />
+                <span className="sender-nickname">{item.sender.nickName}</span>
+              </div>
+              <div className="message-content">
+                {item.type === 0 ? (
+                  item.content
+                ) : item.type === 1 ? (
+                  <img src={item.content} style={{ maxWidth: 200 }} />
+                ) : (
+                  <div>
+                    <a download href={item.content}>
+                      {item.content}
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
+          );
+        })}
+        <div id="bottom-bar" key="bottom-bar"></div>
+      </div>
+      <div className="message-input">
+        <div className="message-type">
+          <div className="message-type-item" key={1}>
+            <Popover
+              content={
+                <EmojiPicker
+                  data={data}
+                  onEmojiSelect={(emoji: any) => {
+                    setInputText((inputText) => inputText + emoji.native);
+                  }}
+                />
+              }
+              title="Title"
+              trigger="click"
+            >
+              表情
+            </Popover>
+          </div>
+          <div
+            className="message-type-item"
+            key={2}
+            onClick={() => {
+              setUploadType("image");
+              setUploadModalOpen(true);
+            }}
+          >
+            图片
+          </div>
+          <div
+            className="message-type-item"
+            key={3}
+            onClick={() => {
+              setUploadType("file");
+              setUploadModalOpen(true);
+            }}
+          >
+            文件
+          </div>
         </div>
-        <UploadModal isOpen={isUploadModalOpen} type={uploadType} handleClose={(fileUrl) => {
-            setUploadModalOpen(false);
+        <div className="message-input-area">
+          <TextArea
+            className="message-input-box"
+            value={inputText}
+            onChange={(e) => {
+              setInputText(e.target.value);
+            }}
+          />
+          <Button
+            className="message-send-btn"
+            type="primary"
+            onClick={() => {
+              sendMessage(inputText);
+              setInputText("");
+            }}
+          >
+            发送
+          </Button>
+        </div>
+      </div>
+      <UploadModal
+        isOpen={isUploadModalOpen}
+        type={uploadType}
+        handleClose={(fileUrl) => {
+          setUploadModalOpen(false);
 
-            if(fileUrl) {
-                sendMessage(fileUrl, uploadType)
-            }
-        }} />
+          if (fileUrl) {
+            sendMessage(fileUrl, uploadType);
+          }
+        }}
+      />
     </div>
+  );
 }
--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,73 +1,82 @@
-import { UserService } from './../user/user.service';
-import { ChatHistory } from '@prisma/client';
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { ChatService } from './chat.service';
-import { Server, Socket } from 'socket.io';
-import { ChatHistoryService } from 'src/chat-history/chat-history.service';
-import { Inject } from '@nestjs/common';
+import { UserService } from "./../user/user.service";
+import { ChatHistory } from "@prisma/client";
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { ChatService } from "./chat.service";
+import { Server, Socket } from "socket.io";
+import { ChatHistoryService } from "src/chat-history/chat-history.service";
+import { Inject } from "@nestjs/common";
 
 interface JoinRoomPayload {
-  chatroomId: number
-  userId: number
+  chatroomId: number;
+  userId: number;
 }
 
 interface SendMessagePayload {
   sendUserId: number;
   chatroomId: number;
   message: {
-    type: 'text' | 'image' | 'file',
-    content: string
-  }
+    type: "text" | "image" | "file";
+    content: string;
+  };
 }
 
-@WebSocketGateway({cors: { origin: '*' }})
+// 订阅两种消息类型：joinRoom 和 sendMessage
+@WebSocketGateway({ cors: { origin: "*" } })
 export class ChatGateway {
+  @Inject(ChatHistoryService)
+  private chatHistoryService: ChatHistoryService;
+
+  @Inject(UserService)
+  private userService: UserService;
+
   constructor(private readonly chatService: ChatService) {}
 
   @WebSocketServer() server: Server;
 
-  @SubscribeMessage('joinRoom')
+  // 客户端发送 emit("joinRoom") 时触发
+  @SubscribeMessage("joinRoom")
   joinRoom(client: Socket, payload: JoinRoomPayload): void {
     const roomName = payload.chatroomId.toString();
 
-    client.join(roomName)
+    client.join(roomName);
 
-    this.server.to(roomName).emit('message', {
-      type: 'joinRoom',
-      userId: payload.userId
+    // 服务端向客户端发送消息
+    this.server.to(roomName).emit("message", {
+      type: "joinRoom",
+      userId: payload.userId,
     });
   }
 
-  @Inject(ChatHistoryService)
-  private chatHistoryService: ChatHistoryService
-
-  @Inject(UserService)
-  private userService: UserService
-  
-  @SubscribeMessage('sendMessage')
+  // 客户端发送 emit("sendMessage") 时触发
+  @SubscribeMessage("sendMessage")
   async sendMessage(@MessageBody() payload: SendMessagePayload) {
     const roomName = payload.chatroomId.toString();
 
     const map = {
       text: 0,
       image: 1,
-      file: 2
-    }
+      file: 2,
+    };
     const history = await this.chatHistoryService.add(payload.chatroomId, {
       content: payload.message.content,
       type: map[payload.message.type],
       chatroomId: payload.chatroomId,
-      senderId: payload.sendUserId
+      senderId: payload.sendUserId,
     });
     const sender = await this.userService.findUserDetailById(history.senderId);
 
-    this.server.to(roomName).emit('message', { 
-      type: 'sendMessage',
+    this.server.to(roomName).emit("message", {
+      type: "sendMessage",
       userId: payload.sendUserId,
       message: {
         ...history,
-        sender
-      }
+        sender,
+      },
     });
   }
 }
